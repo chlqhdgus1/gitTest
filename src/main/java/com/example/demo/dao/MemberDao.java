3ec985e79c8303964b25c9a74232619e9d19ce41
cys92222 <cys92222@naver.com>
2020-03-31 15:10:44 +0900

    ㅁㅁㅁㅁㅁ

--- src/main/java/com/example/demo/dao/MemberDao.java
+++ src/main/java/com/example/demo/dao/MemberDao.java
@@ -2,13 +2,14 @@ package com.example.demo.dao;
 
 public class MemberDao {
 
-	public void listMember() {
-		System.out.println("리스트");
+	public void login() {
+		System.out.println("로그인하였습니다");
 	}
 	
 	public void insertMember() {
-		System.out.println("회원의 정보를 수정했습니다");
+		System.out.println("회원을 등록하였습니다");
 	}
+	
 	
 	public void updateMember() {
 		System.out.println("회원의 정보를 수정했습니다");
