a2dbe691c538f6521b125880bab518a3b50510b5
cys92222 <cys92222@naver.com>
2020-03-31 14:56:47 +0900

    deleteMember 추가

--- src/main/java/com/example/demo/dao/MemberDao.java
+++ src/main/java/com/example/demo/dao/MemberDao.java
@@ -3,10 +3,14 @@ package com.example.demo.dao;
 public class MemberDao {
 
 	public void insertMember() {
-		System.out.println("일해라");
+		System.out.println("회원의 정보를 수정했습니다");
 	}
 	
 	public void updateMember() {
-		System.out.println("회원의 정보를 추가하였습니다");
+		System.out.println("회원의 정보를 수정했습니다");
+	}
+	
+	public void deleteMember() {
+		System.out.println("회원의 정보를 삭제하였습니다");
 	}
 }
