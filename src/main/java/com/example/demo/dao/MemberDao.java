231fd87f7503e23a6f7ce981ee1a8469e7d13f26
susu04-kor <suin0304_sw@google.com>
2020-03-31 14:56:52 +0900

    deleteMember추가함

--- src/main/java/com/example/demo/dao/MemberDao.java
+++ src/main/java/com/example/demo/dao/MemberDao.java
@@ -3,11 +3,15 @@ package com.example.demo.dao;
 public class MemberDao {
 
 	public void insertMember() {
-		System.out.println("일해라");
+		System.out.println("회원의 정보를 수정했습니다.");
 	}
 	
 	public void updateMember() {
-		System.out.println("업데이트합니다아.");
+		System.out.println("회원의 정보를 수정했습니다..");
+	}
+	
+	public void deleteMember() {
+		System.out.println("회원의 정보를 삭제하였습니다.");
 	}
 	
 }
